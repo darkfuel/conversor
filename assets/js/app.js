b77d2bd463cb178ccd8a320148322ad534995d0f
Abel Rivas Bautista <abel.rivas@Imperial.local>
2024-02-18 16:34:56 -0300

    grafico #1

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,31 +9,66 @@ const value = async () => {
     return await response.json()
 }
 
+async function createChart() {
+    const res = await fetch('https://mindicador.cl/api/dolar')
+    const fechas = await res.json()
+
+    const labelsF = fechas.serie.map((date) => {
+        
+        return date.fecha.slice(5, 10)
+    })
+    const labels = labelsF.slice(0, 10)
+    const labelsV = fechas.serie.map((tasa) => {
+        return tasa.valor
+    })
+    const ultiValor = labelsV.slice(0, 10)
+    
+    const datasets = [
+        {   label: "Histórico",
+            borderColor: "rgb(255,99,132",
+            data: ultiValor
+        }
+    ]
+     console.log(labels)
+     console.log(ultiValor)
+    // console.log(datasets)
+    return { labels, datasets }
+}
+
+async function renderChart() {
+    const data = await createChart()
+    console.log(data)
+    const config = {
+        type: 'line',
+        data
+    };
+    new Chart(graph, config)
+}
+
+
 const renderCoin = (coin) => {
     let vPeso = input.value
-    if (selec.value === 'Dolar'){
+    if (selec.value === 'Dolar') {
         const vDolar = coin.dolar.valor
         let vResult = vPeso * vDolar
         result.innerHTML = vResult
-    } else{
+    } else {
         const vEuro = coin.euro.valor
         let vResult = vPeso * vEuro
         result.innerHTML = vResult
-    } 
+    }
 }
 
 const main = async () => {
-   try {
-    const coin =  await value()
-    renderCoin(coin)
-   } catch (error) {
-    alert('Ocurrió un error, verifique conexión y estado de mindicador.cl')
-   }
-    
+    try {
+        const coin = await value()
+        renderChart()
+        createChart()
+        renderCoin(coin)
+    } catch (error) {
+        alert('Ocurrió un error, verifique conexión y estado de mindicador.cl')
+    }
 }
-
 bt.addEventListener('click', () => {
     main()
 })
-
-
